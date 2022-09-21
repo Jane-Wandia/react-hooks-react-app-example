048bd5306be17a1134f762ddbbe16b66a6b8a66e
janekigenyi <janekigenyi@gmail.com>
2022-09-21 20:47:56 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
+import TestComponent from "./TestComponent";
+
 
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>'Now'</h1>
+      <h1>Now</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -18,4 +20,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
